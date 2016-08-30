88940e0fee6e0e881eae720ac92295873a5eef79
Justin Reich <justinreich.dev@gmail.com>
2016-08-30 14:52:28 -0500

    Added isset and other filters to allow user to specify range for the game

--- highlow.php
+++ highlow.php
@@ -1,10 +1,30 @@
 <?php 
 
-$answer = mt_rand(1, 100);
+$minNum = 1;
+$maxNum = 100;
+
+$num1 = (isset($argv[1])) ? $argv[1] : $minNum;
+$num2 = (isset($argv[2])) ? $argv[2] : $maxNum;
+
+if (is_numeric($num1) == 1 && is_numeric($num2) == 1) {
+    $newArray = [$num1, $num2];
+    print_r($newArray);
+    sort($newArray);
+    print_r($newArray);
+    $minNum = $newArray[0];
+    $maxNum = $newArray[1];
+} else {
+    echo "Arguments passed are invalid. Running game with default values." . PHP_EOL;
+}
+
+echo $minNum  . ' and ' . $maxNum . PHP_EOL;
+
+
+$answer = mt_rand($minNum, $maxNum);
 
 fwrite(STDOUT, 'Welcome to the High/Low Game!!!' . PHP_EOL);
 
-fwrite(STDOUT, 'Please guess the number I am thinking of between 1 and 100' . PHP_EOL);
+fwrite(STDOUT, 'Please guess the number I am thinking of between ' . $minNum . ' and ' . $maxNum . PHP_EOL);
 
 $input = null;
 $guesses = null;
